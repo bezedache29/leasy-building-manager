120882173d2b9102647235d8ddedeb6f70e00062
Bezedache <bezedache29@gmail.com>
2026-02-21 19:54:48 +0100

    feat: setup prettier for php and tsx + setup appserviceprovider for https + setup tailwindcss 4.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,37 @@
-import js from "@eslint/js";
-import ts from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
 
 export default [
-    js.configs.recommended,
-    {
-        files: ["**/*.{ts,tsx}"],
-        languageOptions: {
-            parser: tsParser,
-            ecmaVersion: "latest",
-            sourceType: "module",
-        },
-        plugins: {
-            "@typescript-eslint": ts,
-            react: react,
-            "react-hooks": reactHooks,
-        },
-        rules: {
-            "react/react-in-jsx-scope": "off",
-            "@typescript-eslint/no-explicit-any": "error",
-            "no-console": "warn",
-            "react-hooks/rules-of-hooks": "error",
-            "react-hooks/exhaustive-deps": "warn",
-        },
-        settings: {
-            react: { version: "detect" },
-        },
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        route: 'readonly',
+      },
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
+    plugins: {
+      '@typescript-eslint': ts,
+      react: react,
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-console': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
 ];
