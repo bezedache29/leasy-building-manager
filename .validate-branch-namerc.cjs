28295e21527a289a0feb9b991bb01da29bf5042f
Bezedache <bezedache29@gmail.com>
2026-02-21 16:17:27 +0100

    feat: setup husky + lint-staged + validate-branch-name

---
+++ .validate-branch-namerc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    pattern:
+        "^(main|staging|dev)$|^(feature|fix|chore|refactoring)/[a-zA-Z0-9-]+$",
+    errorMsg:
+        "🙄 Mauvais format de branche. Utilise : <type>/<description-sans-espace>",
+};
